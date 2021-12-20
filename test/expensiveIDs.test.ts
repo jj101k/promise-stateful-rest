4939c631bcc493a89ff65cd46a7c8866b93f18c0
Jim Driscoll <jim.a.driscoll@gmail.com>
2021-12-20 21:20:52 +0000

    Centralise the choice of delay

--- test/expensiveIDs.test.ts
+++ test/expensiveIDs.test.ts
@@ -4,6 +4,8 @@ import { Book } from "./lib/Book"
 import { BatchBookCollection } from "./lib/BatchBookCollection"
 const { registerSuite } = intern.getPlugin("interface.object")
 
+const delayMs = 50
+
 registerSuite("Example 2 tests", {
     "Can load a book collection (iterate)": async () => {
         const collection = new BatchBookCollection("/book")
@@ -15,9 +17,9 @@ registerSuite("Example 2 tests", {
             itemsRetained.push(item)
         }
         assert(itemsRetained.length == 3)
-        await new Promise(resolve => setTimeout(resolve, 50))
+        await new Promise(resolve => setTimeout(resolve, delayMs))
         for(const item of itemsRetained) {
-            assert(item.pages?.[0]?.match(/Lorem ipsum/), "pages is available after 50ms")
+            assert(item.pages?.[0]?.match(/Lorem ipsum/), "pages is eventually available")
         }
     },
     "Can load a book collection (at-once)": async () => {
@@ -28,16 +30,16 @@ registerSuite("Example 2 tests", {
             assert(item.name == "Test", "name is immediately available")
             assert(item.pages === undefined, "pages is not immediately available")
         }
-        await new Promise(resolve => setTimeout(resolve, 50))
+        await new Promise(resolve => setTimeout(resolve, delayMs))
         for(const item of itemsRetained) {
-            assert(item.pages?.[0]?.match(/Lorem ipsum/), "pages is available after 50ms")
+            assert(item.pages?.[0]?.match(/Lorem ipsum/), "pages is eventually available")
         }
     },
     "Can load a book": async () => {
         const collection = new BatchBookCollection("/book")
         const itemInitial = await collection.get(1)
         assert(itemInitial === undefined, "Item is initially unset")
-        await new Promise(resolve => setTimeout(resolve, 50))
+        await new Promise(resolve => setTimeout(resolve, delayMs))
         const item = await collection.get(1)
         assert(item !== undefined, "Item is eventually set")
         assert(item.id == 1, "ID matches")
