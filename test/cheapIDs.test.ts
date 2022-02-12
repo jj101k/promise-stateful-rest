d4a89456c96090f0e9d66f0864cbb19bf81ba983
Jim Driscoll <jim.a.driscoll@gmail.com>
2022-02-12 10:26:37 +0000

    Test with a plain map instead of a collection

--- test/cheapIDs.test.ts
+++ test/cheapIDs.test.ts
@@ -1,7 +1,6 @@
 import intern from "intern"
 import assert from "assert"
 import { BatchBook } from "./lib/BatchBook"
-import { BookCollection } from "./lib/BookCollection"
 
 const { registerSuite } = intern.getPlugin("interface.object")
 
@@ -13,19 +12,19 @@ registerSuite("Example 1 tests", {
     },
     tests: {
         "Can load a book collection without initial items": async () => {
-            const collection = new BookCollection()
-            const items = await collection.getAll()
+            const collection = new Map<string, BatchBook>()
             let count = 0
-            for(const item of items) {
+            for(const item of collection.values()) {
                 count++
             }
             assert(count == 0)
         },
         "Can load a book collection with one initial item (number)": async function() {
-            const collection = new BookCollection([42])
-            const items = await collection.getAll()
+            const collection = new Map(
+                [42].map(id => [id, new BatchBook(id)])
+            )
             let count = 0
-            for(const item of items) {
+            for(const item of collection.values()) {
                 count++
                 assert(item.id === 42, "ID matches")
                 assert(item.name === undefined, "Name is ininitally undefined")
@@ -35,10 +34,11 @@ registerSuite("Example 1 tests", {
             assert(count == 1)
         },
         "Can load a book collection with one initial item (string)": async function() {
-            const collection = new BookCollection(["42"])
-            const items = await collection.getAll()
+            const collection = new Map(
+                ["42"].map(id => [id, new BatchBook(id)])
+            )
             let count = 0
-            for(const item of items) {
+            for(const item of collection.values()) {
                 count++
                 assert(item.id === "42", "ID matches")
                 assert(item.name === undefined, "Name is ininitally undefined")
@@ -48,10 +48,11 @@ registerSuite("Example 1 tests", {
             assert(count == 1)
         },
         "Can load a book collection with several items": async function() {
-            const collection = new BookCollection(["42", "44", "45"])
-            const items = await collection.getAll()
+            const collection = new Map(
+                ["42", "44", "45"].map(id => [id, new BatchBook(id)])
+            )
             const seen = new Map<string | number, number>()
-            for(const item of items) {
+            for(const item of collection.values()) {
                 const t = seen.get(item.id) ?? 0
                 seen.set(item.id, t + 1)
                 assert(item.name === undefined, "Name is ininitally undefined for " + item.id)
@@ -62,12 +63,14 @@ registerSuite("Example 1 tests", {
             assert(seen.get("45") === 1)
 
             await new Promise(resolve => setTimeout(resolve, 40))
-            for(const item of items) {
+            for(const item of collection.values()) {
                 assert(item.name == "Some Name", "After 40ms, name matches")
             }
         },
         "Can load a book": async () => {
-            const collection = new BookCollection([1, 2, 3])
+            const collection = new Map(
+                [1, 2, 3].map(id => [id, new BatchBook(id)])
+            )
             const item = await collection.get(1)
             assert(item !== undefined, "Item is initially set")
             assert(item.id == 1, "ID matches")
--- test/lib/BookCollection.ts
+++
@@ -1,8 +0,0 @@
-import { Collected, Type } from "../.."
-import { BatchBook } from "./BatchBook"
-
-export class BookCollection extends Collected.All.Preloaded<BatchBook> {
-    protected newItem(id: Type.id) {
-        return new BatchBook(id)
-    }
-}
